8c7fd83bec120fb933acc98634a2ed79d3b7208d
Frank17 <83507659+Frank17@users.noreply.github.com>
2021-11-05 17:25:52 -0700

    Update Stage 4.py

--- Python/Password Hacker/Stage 4.py
+++ Python/Password Hacker/Stage 4.py
@@ -1,6 +1,7 @@
-# before running the program, download the file used from here:
+# before running the program, download the file used below from here:
 #       https://stepik.org/media/attachments/lesson/255258/logins.txt
 # then save it as logins.txt and upload it to the same directory of execution
+
 import socket
 import sys
 import json
