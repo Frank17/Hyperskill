bccd379815a92db112cd5a45553245387f3f61c6
Frank17 <83507659+Frank17@users.noreply.github.com>
2022-01-07 16:45:09 -0800

    Create Stage 3.py

---
+++ Python/Create Glowing Bacteria/Stage 3.py
@@ -0,0 +1,28 @@
+class Strand:
+    def __init__(self, strand):
+        self.strand = strand
+        self.bases = {'A': 'T',
+                      'T': 'A',
+                      'C': 'G',
+                      'G': 'C'}
+    
+    def get_complementary(self):
+        return ''.join(self.bases[e] for e in self.strand)
+
+    def _insert(self, strand, pos):
+        return strand[:pos] + ' ' + strand[pos:]
+
+    def cut(self, site):
+        comp_strand = self.get_complementary()
+        spos, slen = self.strand.index(site), len(site)
+        return (self._insert(self.strand, spos+1),
+                self._insert(comp_strand, spos+slen-1))
+
+    
+def main():
+    strand = Strand(input())
+    print(strand.strand)
+    print(strand.get_complementary())
+
+if __name__ == '__main__':
+    main()
